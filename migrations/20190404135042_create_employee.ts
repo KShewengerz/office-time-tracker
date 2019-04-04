643d744a2ea434aacb7ffee2d817fce5707f3350
Kristy Mae Almuete <kristyalmuete@gmail.com>
2019-04-04 14:15:27 +0800

    #3 Setup Knex Migration for Employee Table. Add Latest and Rollback Npm Commands on package.json

---
+++ migrations/20190404135042_create_employee.ts
@@ -0,0 +1,20 @@
+import * as Knex from "knex";
+
+import { EmployeeTable } from '@app/enums';
+
+
+export async function up(knex: Knex) {
+  return await knex.schema.createTable(EmployeeTable.Table, table => {
+    table.increments(EmployeeTable.Id).primary();
+    table.string(EmployeeTable.Name).notNullable();
+    table.dateTime(EmployeeTable.ClockIn);
+    table.dateTime(EmployeeTable.ClockOut);
+    table.boolean(EmployeeTable.Active);
+  });
+}
+
+
+export async function down(knex: Knex) {
+  return await knex.schema.dropTable(EmployeeTable.Table);
+}
+
---
+++ shared/enums/tables/employee.ts
@@ -0,0 +1,8 @@
+export enum EmployeeTable {
+  Table     = 'employee',
+  Id        = 'id',
+  Name      = 'name',
+  ClockIn   = 'clock_in',
+  ClockOut  = 'clock_out',
+  Active    = 'active'
+}
